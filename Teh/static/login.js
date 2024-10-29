d412067d326aff79f1ecd0b62a0d47ca0b04afd0
Bruno Antonio <173634685+Brunotg09@users.noreply.github.com>
2024-10-29 19:55:35 -0300

    Delete Teh directory

--- Teh/static/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    localStorage.setItem('authenticated', true);
-    window.location.href = 'dashboard';
-  });
-  
